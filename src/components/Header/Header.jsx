02df4d81fb433f195fff5f37818a6fcf0a567712
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-05-21 14:42:05 -0300

    Head fix responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import './header.scss';
-import TwitterLogo from '../../assets/img/twitter-logo.png'
+import TwitterLogo from '../../assets/img/twitter-logo.png';
+import Headroom from 'react-headroom'
 
 const Header = () => {
     return ( 
         <div className="header">
+             <Headroom style={{
+               alignContent:'center',
+               transition: 'all .5s ease-in-out',
+               alignItems:'center',
+               textAlign:"center"
+               
+               }}>
              <img src={TwitterLogo} alt="Tweet Simulator"/>
              <h1>Tweets simulator</h1>
+             </Headroom>
         </div>
      );
 }
